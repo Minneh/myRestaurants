50ae63a21b06f8aa3cc192a173d583386350088d
Minneh Mugo <minneh.mugo@gmail.com>
2017-06-11 11:45:38 +0300

    add interfaces for touchscreen events

---
+++ app/src/main/java/util/ItemTouchHelperAdapter.java
@@ -0,0 +1,10 @@
+package util;
+
+/**
+ * Created by minnehmugo on 11/06/2017.
+ */
+
+public interface ItemTouchHelperAdapter {
+    boolean onItemMove(int fromPosition, int toPosition);
+    void onItemDismiss(int position);
+}
---
+++ app/src/main/java/util/OnStartDragListener.java
@@ -0,0 +1,11 @@
+package util;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by minnehmugo on 11/06/2017.
+ */
+
+public interface OnStartDragListener {
+    void onStartDrag(RecyclerView.ViewHolder viewHolder);
+}
