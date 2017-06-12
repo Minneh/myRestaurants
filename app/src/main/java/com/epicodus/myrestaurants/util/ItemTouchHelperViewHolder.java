eaa2d755a24d3a7863e850b34b5e432094c1a029
Minneh Mugo <minneh.mugo@gmail.com>
2017-06-12 14:28:59 +0300

    create ItemTouchHelperViewHolder interface in util to define methods that will be called when an item is selected for a drag-and-drop gesture

---
+++ app/src/main/java/com/epicodus/myrestaurants/util/ItemTouchHelperViewHolder.java
@@ -0,0 +1,10 @@
+package com.epicodus.myrestaurants.util;
+
+/**
+ * Created by minnehmugo on 12/06/2017.
+ */
+
+public interface ItemTouchHelperViewHolder {
+    void onItemSelected();
+    void onItemClear();
+}
